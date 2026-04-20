4af1e1ed9be28d8addd15e12a957565e1c9e179b
hoseony <hoseon.alex.yu@gmail.com>
2026-04-19 20:12:02 -0400

    hopefully it work

--- source/uart.c
+++ source/uart.c
@@ -2,21 +2,95 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "uart.h"
+// #include "hardware/clocks.h"
+
+// --------------- function prototypes -----------------------
+void UART_transmitByte(UART_t *port, uint8_t byte);
+void UART_init(UART_t *port, RESETS_t *resets, uint32_t baudRate);
+void UART_setBaudRate(UART_t *port, uint32_t UART_clk_hz, uint32_t baudRate);
+void UART_transmitBytes(UART_t *port, uint8_t *byte, size_t length);
+int UART_receiveByte(UART_t *port, uint8_t *receiveBuffer, uint32_t *receiveBufferIndex, uint32_t receiveBufferSize);
+int UART_receiveBytes(UART_t *port, uint8_t *receiveBuffer, uint32_t *receiveBufferIndex, uint32_t receiveBufferSize);
+void UART_writeBitFIFO(UART_t *port, bool enable);
+void UART_writeBitCR(UART_t *port, bool enable);
+// -----------------------------------------------------------
 
 int main() {
     int bufSize = 100;
     uint8_t receiveBuffer[bufSize];
     uint32_t receiveBufferIndex = 0;
+
+    UART_init(UART0, RESETS, 115200);
+    
+    for (volatile int i = 0; i < 100000; i++);  // small delay
+   
+    uint8_t string[] = "hello aidan";
+    while(1) {
+        UART_transmitBytes(UART0, string, sizeof(string) - 1);
+    }
 }
 
 // write byte to Data Register
 void UART_transmitByte(UART_t *port, uint8_t byte) {
     while (1) {
-        if ( !(port->UART_FR & TXFF_MASK) ) {
+        if ( !(port->UART_FR & UART_FR_TXFF_MASK) ) {
             port->UART_DR = byte;
             break;
         }
     }
+}
+
+// basically made simpler version of uart_init from pg.972 on rp2350 documentation
+void UART_init(UART_t *port, RESETS_t *resets, uint32_t baudRate) {
+    // initialize the memory address
+    GPIO_t *gpio = (GPIO_t *)BANK_IO_BASE;
+ 
+    if (port == NULL || resets == NULL) {
+        return;
+    }
+
+    // disable bits
+    UART_writeBitCR(port, 0);
+
+    // make UART0 to 0, (using UART)
+    resets->RESET &= ~(RESET_UART0_MASK); // make UART0 to 0
+
+    while( !(resets->RESET_DONE & RESET_UART0_MASK)) {
+        ; // I am waiting for it to be reset
+    }
+   
+    gpio->GPIO0_CTRL = GPIO_UART_FUNCT;
+    gpio->GPIO1_CTRL = GPIO_UART_FUNCT;
+
+    UART_setBaudRate(port, 48000000u, baudRate);
+    UART_writeBitFIFO(port, 1);
+
+    port->UART_LCR_H &= ~(0x3u << 5); // clear stuff
+    port->UART_LCR_H |= (0x3u << 5); // setting the word length to be 8 ('b11 = 0x3u)
+
+    UART_writeBitCR(port, 1);
+}
+
+void UART_setBaudRate(UART_t *port, uint32_t UART_clk_hz, uint32_t baudRate) {
+    uint32_t baud_rate_div = (8 * UART_clk_hz / baudRate) + 1;
+    uint32_t baud_ibrd = baud_rate_div >> 7;
+    uint32_t baud_fbrd;
+
+    if (baud_ibrd == 0) {
+        baud_ibrd = 1;
+        baud_fbrd = 0;
+    } else if (baud_ibrd >= 65535) {
+        baud_ibrd = 65535;
+        baud_fbrd = 0;
+    } else {
+        baud_fbrd = (baud_rate_div & 0x7f) >> 1;
+    }
+
+    port->UART_IBRD = baud_ibrd;
+    port->UART_FBRD = baud_fbrd; 
+
+    port->UART_IBRD = 26;
+    port->UART_FBRD = 3;
 }
 
 // write bytes to Data Register
@@ -24,7 +98,7 @@ void UART_transmitBytes(UART_t *port, uint8_t *byte, size_t length) {
     if (byte == NULL) return;
 
     while(length != 0) {
-        if ( !(port->UART_FR & TXFF_MASK) ) {
+        if ( !(port->UART_FR & UART_FR_TXFF_MASK) ) {
             port->UART_DR = *byte;
             byte++;
             length--;
@@ -35,11 +109,21 @@ void UART_transmitBytes(UART_t *port, uint8_t *byte, size_t length) {
 // write FIFO bit on LCR_H register (to enable / disbale FIFO)
 void UART_writeBitFIFO(UART_t *port, bool enable) {
     if (enable) {
-        port->UART_LCR_H |= (FEN_MASK);
+        port->UART_LCR_H |= (UART_LCR_H_FEN_MASK);
     } else {
-        port->UART_LCR_H &= ~(FEN_MASK);
+        port->UART_LCR_H &= ~(UART_LCR_H_FEN_MASK);
     }
 }
+
+// enable RXE, TXE, EN for CR (you need to do this for initialize)
+void UART_writeBitCR(UART_t *port, bool enable) {
+    if (enable) {
+        port->UART_CR |= (UART_CR_RXE_MASK | UART_CR_TXE_MASK | UART_CR_EN_MASK);
+    } else {
+        port->UART_CR &= ~(UART_CR_RXE_MASK | UART_CR_TXE_MASK | UART_CR_EN_MASK);
+    }
+}
+
 
 int UART_receiveByte(UART_t *port, uint8_t *receiveBuffer, uint32_t *receiveBufferIndex, uint32_t receiveBufferSize) {
     if (port == NULL || receiveBuffer == NULL || receiveBufferIndex == NULL) {
@@ -47,7 +131,7 @@ int UART_receiveByte(UART_t *port, uint8_t *receiveBuffer, uint32_t *receiveBuff
         return 1;
     }
 
-    while (port->UART_FR & RXFE_MASK) {
+    while (port->UART_FR & UART_FR_RXFE_MASK) {
         ; // wait until DR is not empty
     }
 
@@ -70,7 +154,7 @@ int UART_receiveBytes(UART_t *port, uint8_t *receiveBuffer, uint32_t *receiveBuf
         return 1;
     }
 
-    while ( !(port->UART_FR & RXFE_MASK) ) { // if DR is not empty
+    while ( !(port->UART_FR & UART_FR_RXFE_MASK) ) { // if DR is not empty
         // you read byte at a time, but UART_DR is 32bit, so you mask the first 8 bits
         uint8_t data = (uint8_t)(port->UART_DR & 0xFF);
 
--- source/uart.h
+++ source/uart.h
@@ -3,11 +3,21 @@
 
 #include <stdint.h>
 
-#define TXFF_MASK (0x00000001 << 5)
-#define FEN_MASK (0x00000001 << 4)
-#define RXFE_MASK (0x00000001 << 4)
+#define UART_FR_TXFF_MASK (0x00000001u << 5)
+#define UART_FR_RXFE_MASK (0x00000001u << 4)
+#define UART_LCR_H_FEN_MASK (0x00000001u << 4)
 
-typedef struct{
+#define UART0_BASE 0x40070000u
+#define UART1_BASE 0x40078000u
+
+#define UART0 ((UART_t *)UART0_BASE)
+#define UART1 ((UART_t *)UART1_BASE)
+
+#define UART_CR_RXE_MASK (0x00000001u << 9) // receive bit (1 to receive)
+#define UART_CR_TXE_MASK (0x00000001u << 8) // transmit bit (1 to transmit)
+#define UART_CR_EN_MASK (0x00000001u << 0) // enable UART (1 to enable)
+
+typedef struct {
     volatile uint32_t UART_DR;          // 0x000 Data Register
     volatile uint32_t UART_SR;          // 0x004 Recieve Status Register (error message)
     volatile uint32_t RESERVED0[4];     // occupy 16 bytes
@@ -15,7 +25,7 @@ typedef struct{
     volatile uint32_t RESERVED1;        // occupy byte
     volatile uint32_t UART_ILPR;        // 0x020 low-power counter register
     volatile uint32_t UART_IBRD;        // 0x024 integer baud rate
-    volatile uint32_t UART_FBRD;         // 0x028 fractional baud rate
+    volatile uint32_t UART_FBRD;        // 0x028 fractional baud rate
     volatile uint32_t UART_LCR_H;       // 0x02c line control register
     volatile uint32_t UART_CR;          // 0x030 Control Register
     volatile uint32_t UART_IFLS;        // 0x034 interrupt FIFO
@@ -24,14 +34,38 @@ typedef struct{
     volatile uint32_t UART_MIS;         // 0x040 Masked Interrupt status register
     volatile uint32_t UART_ICR;         // 0x044 Interrupt clear register
     volatile uint32_t UART_DMACR;       // 0x048 DMA control Register
-    volatile uint32_t UART_PERIPHID0;   // peripherals
+/*    volatile uint32_t UART_PERIPHID0;   // peripherals
     volatile uint32_t UART_PERIPHID1;
     volatile uint32_t UART_PERIPHID2;
     volatile uint32_t UART_PERIPHID3;
     volatile uint32_t UART_PCELLID0;    // PCELL?
     volatile uint32_t UART_PCELLID1;
     volatile uint32_t UART_PCELLID2;
-    volatile uint32_t UART_PCELLID3;
+    volatile uint32_t UART_PCELLID3;*/
 } UART_t;
+
+
+#define RESET_UART0_MASK (0x00000001u << 26)
+#define RESET_UART1_MASK (0x00000001u << 27)
+#define RESET_BASE 0x40020000u
+#define RESETS ((RESETS_t *)RESET_BASE)
+
+typedef struct {
+    volatile uint32_t RESET; // 0x40020000
+    volatile uint32_t WDSEL;
+    volatile uint32_t RESET_DONE;
+} RESETS_t;
+
+// ------------------------------
+
+#define GPIO_UART_FUNCT 2 
+#define BANK_IO_BASE 0x40028000u
+
+typedef struct {
+    volatile uint32_t GPIO0_STATUS; //0x40028000
+    volatile uint32_t GPIO0_CTRL;
+    volatile uint32_t GPIO1_STATUS;
+    volatile uint32_t GPIO1_CTRL;
+} GPIO_t;
 
 #endif
